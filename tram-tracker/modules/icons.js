2c702ab5d119cd55c025081d7ac69d0352664500
Anders Wittfeldt <anders@lolo.company>
2022-02-06 18:23:23 +0100

    Color fixes

--- tram-tracker/modules/icons.js
+++ tram-tracker/modules/icons.js
@@ -1,29 +1,36 @@
 const tram = (text, bgColor) => {
   const textX = text.length > 1 ? 4 : 10;
+  const textColor = getTextColor(bgColor);
 
   return `
     <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
      <g>
       <rect rx="8" stroke-width="0" stroke="#000" height="32" width="32" y="0" x="0" fill="${bgColor}"/>
-      <text font-family="Caladea" font-size="24" y="24" x="${textX}" stroke-width="0" stroke="#000" fill="#ffffff">${text}</text>
+      <text font-family="Caladea" font-size="24" y="24" x="${textX}" stroke-width="0" fill="${textColor}">${text}</text>
      </g>
     </svg>
   `;
 }
 
 const other = (text, bgColor) => {
-  const rows = text.split(' ');
+  const rows = text.split(' ').filter(row => row);
+  const textColor = getTextColor(bgColor);
 
   return `
     <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
      <g>
       <rect rx="8" stroke-width="0" stroke="#000" height="32" width="32" y="0" x="0" fill="${bgColor}"/>
-      <text font-family="Caladea" font-size="13" y="14" x="4" stroke-width="0" stroke="#000" fill="#ffffff">${rows[0]}</text>
-      <text font-family="Caladea" font-size="13" y="26" x="4" stroke-width="0" stroke="#000" fill="#ffffff">${rows[1]}</text>
+      <text font-family="Caladea" font-size="13" y="14" x="4" stroke-width="0" fill="${textColor}">${rows[0]}</text>
+      <text font-family="Caladea" font-size="13" y="26" x="4" stroke-width="0" fill="${textColor}">${rows[1]}</text>
      </g>
     </svg>
   `;
 };
+
+const getTextColor = bgColor => {
+  if (bgColor.startsWith('#FF')) return '#000000';
+  return '#FFFFFF';
+}
 
 const getIcon = (name, bgColor) => {
   const m = name.match(/^Spå ([0-9]+)$/);
